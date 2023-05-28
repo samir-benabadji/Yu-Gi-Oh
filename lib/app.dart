0828fdf51eac951446cd563ac31f7dc96b379dab
samir-benabadji <78525925+samir-benabadji@users.noreply.github.com>
2023-05-28 11:59:50 +0100

    Working with dynamic themes

---
+++ lib/app.dart
@@ -0,0 +1,44 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
+
+import 'package:flutter/material.dart';
+
+import 'presentation/constants/themes.dart';
+import 'presentation/loading_view/loading_state_info.dart';
+import 'router.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _loadingState = LoadingStateInfo();
+
+  late final _router = routerGenerator(loadingState: _loadingState);
+
+  @override
+  void dispose() {
+    _router.dispose();
+    _loadingState.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DynamicTheme(
+      defaultThemeMode: ThemeMode.dark,
+      themedWidgetBuilder: (_, themeMode, __) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          themeMode: themeMode,
+          darkTheme: MyThemes.dark,
+          theme: MyThemes.dark,
+          routeInformationParser: _router.routeInformationParser,
+          routerDelegate: _router.routerDelegate,
+        );
+      },
+    );
+  }
+}
