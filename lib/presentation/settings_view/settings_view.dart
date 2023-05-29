63209be3b01abe0a495ecfe2286cd2a60e9487bc
samir-benabadji <78525925+samir-benabadji@users.noreply.github.com>
2023-05-29 11:29:38 +0100

    Working on the settings view

---
+++ lib/presentation/settings_view/settings_view.dart
@@ -0,0 +1,80 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flutter/material.dart';
+
+import '../../core/consts/consts.dart';
+import '../../core/consts/my_edge_insets.dart';
+import '../components/rounded_scaffold_box.dart';
+import '../constants/themes.dart';
+
+class _ThemeChoice {
+  final String name;
+  final ThemeMode mode;
+
+  const _ThemeChoice({required this.name, required this.mode});
+}
+
+const _themeItems = <_ThemeChoice>[
+  _ThemeChoice(name: 'Light', mode: ThemeMode.light),
+  _ThemeChoice(name: 'Dark', mode: ThemeMode.dark),
+  _ThemeChoice(name: 'System', mode: ThemeMode.system),
+];
+
+class SettingsView extends StatefulWidget {
+  const SettingsView({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  late _ThemeChoice _themeChoice = _themeItems
+      .firstWhere((item) => item.mode == DynamicTheme.of(context).themeMode);
+
+  void _changeBrightness(_ThemeChoice? themeChoice) {
+    if (themeChoice == null) return;
+    setState(() => _themeChoice = themeChoice);
+    MyThemes.changeBrightness(
+      context,
+      themeMode: _themeChoice.mode,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        centerTitle: true,
+      ),
+      body: RoundedScaffoldBox(
+        child: ListView(
+          padding: MyEdgeInsets.all16,
+          children: [
+            Row(
+              children: [
+                const Icon(Icons.light_mode),
+                const SizedBox(width: Consts.px8),
+                const Text('Theme'),
+                const SizedBox(width: Consts.px16),
+                Expanded(
+                  child: DropdownButton<_ThemeChoice>(
+                    value: _themeChoice,
+                    onChanged: _changeBrightness,
+                    items: _themeItems
+                        .map<DropdownMenuItem<_ThemeChoice>>(
+                          (e) => DropdownMenuItem(
+                            value: e,
+                            child: Text(e.name),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
