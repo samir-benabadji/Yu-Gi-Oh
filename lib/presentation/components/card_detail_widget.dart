5e45526ecdde7a84eb82ca2805bc6f47f411ace7
Samir Benabadji <benabadji.samir.2002@gmail.com>
2023-06-19 12:42:37 +0100

    Flutter Card Description and Detail Widget
    The Flutter Card Description and Detail Widget is a powerful and versatile component that combines both descriptive and detailed information within a card. It offers a clean and consistent layout for presenting textual content in an organized manner. The widget includes a description section, allowing you to display a brief summary or explanation, and a detail section, which showcases more specific data with labels and corresponding values. With customizable text styles and the option to include leading widgets like images, this widget provides a comprehensive solution for creating visually appealing cards in your Flutter applications.

---
+++ lib/presentation/components/card_detail_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../../core/consts/consts.dart';
+import '../../core/consts/my_edge_insets.dart';
+import '../constants/text_styles.dart';
+
+class CardDetailWidget extends StatelessWidget {
+  final String label;
+  final String value;
+  final Widget? leading;
+
+  const CardDetailWidget({
+    Key? key,
+    required this.label,
+    required this.value,
+    this.leading,
+  }) : super(key: key);
+
+  factory CardDetailWidget.assetImage({
+    required String label,
+    required String value,
+    required String asset,
+  }) =>
+      CardDetailWidget(
+        label: label,
+        value: value,
+        leading: Image.asset(
+          asset,
+          errorBuilder: (_, error, ___) => const SizedBox(),
+          height: Consts.px20,
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label, style: TextStyles.grey14),
+        const SizedBox(height: Consts.px4),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (leading != null)
+              Padding(
+                padding: MyEdgeInsets.onlyR4,
+                child: leading,
+              ),
+            Flexible(child: Text(value, maxLines: 2)),
+          ],
+        ),
+      ],
+    );
+  }
+}
